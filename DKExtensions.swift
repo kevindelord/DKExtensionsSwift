9f68aa0da9032e4628d41f80c884217b957c4713
kevindelord <delord.kevin@gmail.com>
2015-05-28 17:25:03 +0200

    Add addtitions

--- DKExtensions.swift
+++ DKExtensions.swift
@@ -35,6 +35,26 @@ class PopViewControllerSegue : UIStoryboardSegue {
     }
 }
 
+// MARK: - Additions
+
+func += <KeyType, ValueType> (inout left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
+    for (k, v) in right {
+        left.updateValue(v, forKey: k)
+    }
+}
+
+func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>) -> Dictionary<K,V> {
+
+    var map = Dictionary<K,V>()
+    for (k, v) in left {
+        map[k] = v
+    }
+    for (k, v) in right {
+        map[k] = v
+    }
+    return map
+}
+
 // MARK: - Extensions
 
 extension NSError {
