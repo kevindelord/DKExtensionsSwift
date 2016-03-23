c19d1727d51eb30824cceedadcbbeed478fcc7c5
kevindelord <delord.kevin@gmail.com>
2016-03-23 11:59:33 +0100

    Add allCases extension for Int enums

--- DKExtensions.swift
+++ DKExtensions.swift
@@ -98,6 +98,21 @@ func + <K, V>(left: Dictionary<K, V>, right: Dictionary<K, V>) -> Dictionary<K, 
 
 // MARK: - Extensions
 
+extension RawRepresentable where RawValue == Int {
+	static var allCases: [Self] {
+		// Create array to store all found cases
+		var cases = [Self]()
+		// Use 0 as start index
+		var index = 0
+		// Try to create a case for every following integer index until no case is created. If no one is created,
+		while let _case = self.init(rawValue: index) {
+			cases.append(_case)
+			index += 1
+		}
+		return cases
+	}
+}
+
 extension NSError {
     func log() {
         print("Error: \(self) \(self.userInfo)")
